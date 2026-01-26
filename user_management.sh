f10f702f05b3d3941a9ae9d9213a8b363052e589
melek4000 <melek2maalej@@gmail.com>
2026-01-26 10:52:58 +0000

    user management

--- user_management.sh
+++ user_management.sh
@@ -1,12 +1,66 @@
 #!/bin/bash
 
-# Function to display usage information and available options
-function display_usage {
-    echo "Usage: $0 [OPTIONS]"
-    echo "Options:"
-    echo "  -c, --create     Create a new user account."
-    echo "  -d, --delete     Delete an existing user account."
-    echo "  -r, --reset      Reset password for an existing user account."
-    echo "  -l, --list       List all user accounts on the system."
-    echo "  -h, --help       Display this help and exit."
+function Display_usage {
+echo "Usage: $0 [OPTIONS]"
+echo "-c, --create  create a new user account."
+echo "-d, --delete  delete an existing user account."
+echo "-r, --reset   reset password for an existing user."
+echo "-l, --list    list all user accounts on the system."
+echo "-h, --help    display this help and exit."
 }
+function create_user {
+
+read -p "Enter the new username: " username
+if  id "$username" 1>/dev/null 2>/dev/null  ; then
+echo "Error: the username '$username' already exist.please choose another username."
+else
+read -p "Enter the password for $username: " password
+sudo useradd -p "$password" -m "$username"
+echo "user account '$username' created successfully."  
+fi
+}
+
+function list_users {
+echo "users accounts on the system:"
+cat /etc/passwd | awk -F :  '{print $1 "(UID:" $3 ")"}'
+} 
+function delete_user {
+read -p "Enter the user to delete:" username
+if  id "$username" ; then
+sudo userdel -r "$username"  
+echo "the account '$username' successfully deleted."
+else
+echo "the user $username not exist.please enter a valid user."
+fi
+}
+function reset_password {
+read -p "Enter the username to reset password: " username
+if id "$username" ; then
+read -p "Enter the new password: " password
+echo "$username:$password" | sudo chpasswd
+echo "password for user $username reset successfully."
+else
+echo "the user does not exist."
+fi
+}
+while [ "$#" -gt 0 ]; do
+
+
+case "$1" in 
+-c | --create)
+create_user ;;
+-d | --delete)
+delete_user;;
+-r | --reset)
+reset_password;;
+-l | --list)
+list_users;;
+-h | --help)
+Display_usage;;
+*)
+Display_usage
+exit 1 ;;
+esac
+shift
+done
+ 
